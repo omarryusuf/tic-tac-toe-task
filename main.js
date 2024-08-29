318f8383db3a95748e3bd1e58f81a495c93d2dde
Omar Yusuf <omar.khaled.yusuf@gmail.com>
2024-08-29 18:48:23 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,15 @@
 let title = document.querySelector('.header')
 let boxes = document.querySelectorAll('.box');
-let squares = [];
-function checkWinner()
-{
-  for(let i = 1; i<10;i++)
-    If(squares[1] == squares[2] && squares[2] == squares [3] && squares[1] != '')
-  { squares[i] = document.getElementById('square' + i).innerHTML;}
-}
-
 // game turns & click function
 let turn = 'x';
+let squares = []; 
+
 function game(id)
 {
+
     let element = document.getElementById(id);
+    let my_current_turn = turn;
+
     if(turn === 'x' && element.innerHTML == '')
     {   
         element.innerHTML = 'x';
@@ -25,6 +22,29 @@ function game(id)
         turn = 'x'
         title.innerHTML = 'x turn'
     }
-    checkWinner();
+    let checked_winner = checkWinner(my_current_turn);
+    if(checked_winner == true){
+        turn = "endgame";
+        return;
+    }
 }
 
+function checkWinner(current_turn){
+    let winConditions = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],
+        [0, 4, 8], [2, 4, 6]
+    ]
+    for(let i=0; i<winConditions.length; i++){
+        let v0 = boxes [winConditions[i][0]].innerHTML;
+        let v1 = boxes [winConditions[i][1]].innerHTML;
+        let v2 = boxes [winConditions [i] [2]].innerHTML;
+
+        if(v0 != "" && v0 === v1 && v0 === v2){
+            turn = "endgame";
+            title.innerHTML = 'gameover';
+            return;
+        }
+      
+    }
+}
